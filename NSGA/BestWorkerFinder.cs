0fd9ddefca12feff2c7f5bfb8285db006b8f50a3
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-24 14:36:39 +0100

    created NSGA project

---
+++ NSGA/BestWorkerFinder.cs
@@ -0,0 +1,10 @@
+﻿namespace NSGA
+{
+    internal class BestWorkerFinder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
