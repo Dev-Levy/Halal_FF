f564dc2f246f530bf7a9e3ba5e3aecf397a2b7b3
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-26 23:18:41 +0100

    made the hill climbing work for 3D

--- HillClimbing/FunctionOptimumFinder.cs
+++ HillClimbing/FunctionOptimumFinder.cs
@@ -4,36 +4,36 @@ namespace HillClimbing
 {
     internal class FunctionOptimumFinder
     {
-        static readonly double MAX = 10.0;
-        static readonly double MIN = 0.0;
-        static void Main(string[] args)
+        static void Main()
         {
-            double best = 1.0;
-            Console.WriteLine($"Starting X is: {double.Round(best, 4)} (fitness: {double.Round(f(best), 8)})");
-            for (int i = 0; i < 1000; i++)
+            Console.WriteLine($"Goal to find: [0, 0] {f([0, 0])}");
+            double[] best = [3.0, 3.0];
+            Console.WriteLine($"Starting [X,Y] is: {double.Round(best[0], 4)}, {double.Round(best[1], 4)} (fitness: {double.Round(f(best), 8)})");
+            for (int i = 0; i < 10000; i++)
             {
-                double curr = HillClimbingSteepestAscent(0.0001);
+                double[] curr = HillClimbingSteepestAscent(0.0001);
                 if (f(curr) <= f(best))
                 {
                     best = curr;
-                    Console.WriteLine($"Better X is: {double.Round(best, 4)} (fitness: {double.Round(f(best), 8)})");
+                    Console.WriteLine($"Better [X,Y] is: {double.Round(best[0], 4)}, {double.Round(best[1], 4)} (fitness: {double.Round(f(best), 8)})");
                 }
             }
-            Console.WriteLine($"Absolute best X is: {double.Round(best, 4)} (fitness: {double.Round(f(best), 8)})");
+            Console.WriteLine($"Absolute best [X,Y] is: {double.Round(best[0], 4)}, {double.Round(best[1], 4)} (fitness: {double.Round(f(best), 8)})");
         }
 
-        private static double f(double x)
+        private static double f(double[] point)
         {
-            return Math.Sin(x) + 0.5 * Math.Sin(2 * x) + 0.25 * Math.Sin(3 * x) + 0.5 * Math.Cos(Math.PI * x);
+            return 0.02 * (Math.Pow(point[0], 2) + Math.Pow(point[1], 2)) + -0.2 * Math.Cos(point[0]) + -0.2 * Math.Cos(point[1]);
         }
 
-        private static double HillClimbingSteepestAscent(double epsilon)
+        private static double[] HillClimbingSteepestAscent(double epsilon)
         {
-            double best = Random.Shared.NextDouble() * 10;
+            double[] best = GetRandomPos();
+
             bool stuck = false;
             while (!stuck)
             {
-                double next = SelectMinimum(best, epsilon);
+                double[] next = SelectMinimum(best, epsilon);
 
                 if (f(next) < f(best))
                 {
@@ -47,16 +47,59 @@ namespace HillClimbing
             return best;
         }
 
-        private static double SelectMinimum(double current, double epsilon)
+        private static double[] GetRandomPos()
         {
-            if (f(current + epsilon) < f(current - epsilon))
-            {
-                return Math.Min(current + epsilon, MAX);
-            }
+            double[] pos = [0, 0];
+
+            if (Random.Shared.Next() % 2 == 0)
+                pos[0] = Util.GetDouble();
             else
-            {
-                return Math.Max(current - epsilon, MIN);
-            }
+                pos[0] = -Util.GetDouble();
+
+            if (Random.Shared.Next() % 2 == 0)
+                pos[1] = Util.GetDouble();
+            else
+                pos[1] = -Util.GetDouble();
+
+            return pos;
         }
+
+        private static double[] SelectMinimum(double[] current, double epsilon)
+        {
+            //8 ways decider
+            double[] minimal = current;
+
+            if (f([current[0] + epsilon, current[1] + epsilon]) < f(minimal))   // fel, jobbra
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0], current[1] + epsilon]) < f(minimal))             // fel
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0] - epsilon, current[1] + epsilon]) < f(minimal))   //fel, ballra
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0] - epsilon, current[1]]) < f(minimal))             //ballra
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0] - epsilon, current[1] - epsilon]) < f(minimal))   //le, ballra
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0], current[1] - epsilon]) < f(minimal))             //le
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0] + epsilon, current[1] - epsilon]) < f(minimal))   //le, jobbra
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            if (f([current[0] + epsilon, current[1]]) < f(minimal))             //jobbra
+                minimal = [current[0] + epsilon, current[1] + epsilon];
+
+            return minimal;
+        }
+    }
+    class Util
+    {
+        static readonly int RANGE = 10;
+        static readonly Random RND = new();
+        public static double GetDouble() => RND.NextDouble() * RANGE;
     }
 }
