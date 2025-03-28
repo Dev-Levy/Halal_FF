e96a424b1554c6ef961f0a7f43e0dd1254fc6eb1
Olah Levente <oll4bp@bosch.com>
2025-03-28 16:27:28 +0100

    DBSCAN half done

---
+++ DBSCAN/ClickingPositionClusterer.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DBSCAN
+{
+    internal class ClickingPositionClusterer
+    {
+        static void Main()
+        {
+
+        }
+
+        public List<HashSet<int[]>> DBSCAN(int[][] clicks, int minPts, double eps)
+        {
+            List<HashSet<int[]>> clusters = [];
+            HashSet<int[]> Processed = [];
+
+            foreach (int[] p in clicks)
+            {
+                if (!Processed.Contains(p))
+                {
+                    HashSet<int[]> Neighbours = clicks.Where(other => Ds(p, other) <= eps).ToHashSet();
+                    if (Neighbours.Count >= minPts)
+                    {
+                        HashSet<int[]> Reachable = [];
+                        foreach (int[] q in Neighbours)
+                        {
+                            Processed.Add(q);
+                            Reachable.Add(q);
+                            HashSet<int[]> ClosePoints = clicks.Where(other => Ds(q, other) <= eps).ToHashSet();
+                            if (ClosePoints.Count >= minPts)
+                            {
+                                Neighbours.UnionWith(ClosePoints); // ez problem
+                            }
+                        }
+                        clusters.Add(Reachable);
+                    }
+                }
+            }
+            return clusters;
+        }
+
+        private static double Ds(int[] p, int[] other) => Math.Sqrt(Math.Pow(p[0] - other[0], 2) + Math.Pow(p[1] - other[1], 2));
+    }
+}
---
+++ MouseClickLogger/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Gma.System.MouseKeyHook;
+
+namespace MouseClickLogger
+{
+    class MouseClickLogger
+    {
+        private static IKeyboardMouseEvents mouseEvents;
+        private string logFilePath = "MouseClicksLog.txt";
+
+        public static void StartLogging()
+        {
+            mouseEvents = Hook.GlobalEvents();
+
+            mouseEvents.MouseDown += OnMouseDown;
+
+            Console.WriteLine("Mouse click logger started. Press ENTER to stop...");
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            string logEntry = $"{DateTime.Now}: Mouse clicked at ({e.X}, {e.Y})";
+            Console.WriteLine(logEntry);
+
+            File.AppendAllLines(logFilePath, new[] { logEntry });
+        }
+
+        public static void StopLogging()
+        {
+            if (mouseEvents != null)
+            {
+                mouseEvents.MouseDown -= OnMouseDown;
+                mouseEvents.Dispose();
+            }
+
+            Console.WriteLine("Mouse click logger stopped.");
+        }
+
+        static void Main(string[] args)
+        {
+            var logger = new MouseClickLogger();
+
+            logger.StartLogging();
+
+            Console.ReadLine();
+
+            logger.StopLogging();
+        }
+    }
+}
