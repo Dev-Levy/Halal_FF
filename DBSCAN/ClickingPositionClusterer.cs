0b32c3f048608d009acd5f60d56233d2b734e83b
Olah Levente <oll4bp@bosch.com>
2025-03-31 14:54:04 +0200

    made DBSCAN work and illustrate

--- DBSCAN/ClickingPositionClusterer.cs
+++ DBSCAN/ClickingPositionClusterer.cs
@@ -7,51 +7,116 @@ namespace DBSCAN
 {
     internal class ClickingPositionClusterer
     {
+        static readonly int SCREEN_WIDTH = 1920;
+        static readonly int SCREEN_HEIGHT = 1080;
         static void Main()
         {
-            string logfile = "MouseClicks.txt";
-            int[][] clicks = File.ReadAllLines(logfile).Select(line => new int[] { int.Parse(line.Split(';')[0]), int.Parse(line.Split(';')[1]) }).ToArray();
+            string logfile = "clicks.txt";
+            Point[] clicks = File.ReadAllLines(logfile).Select(line => new Point { X = int.Parse(line.Split(',')[0]), Y = int.Parse(line.Split(',')[1]) }).ToArray();
             int minPts = 3;
-            double eps = 1.0;
-            List<HashSet<int[]>> clusters = DBSCAN(clicks, minPts, eps);
-
+            double eps = 50;
+            List<HashSet<Point>> clusters = DBSCAN(clicks, minPts, eps);
+            Console.WriteLine($"{clusters.Count} clusters found!");
             foreach (var cluster in clusters)
             {
-                Console.Write(cluster.Average(point => point[0]));
-                Console.WriteLine(cluster.Average(point => point[1]));
+                Console.WriteLine($"Cluster position average X: {cluster.Average(p => p.X)} Y: {cluster.Average(p => p.Y)}");
+            }
+            DrawIllustration(clusters);
+        }
+
+        private static void DrawIllustration(List<HashSet<Point>> clusters)
+        {
+            int minimized_x = 64 * 2; //multiply to compensate for chars being not equally tall as wide on console
+            int minimized_y = 36;
+            foreach (var cluster in clusters)
+            {
+                var pos_x = cluster.Average(p => p.X) / SCREEN_WIDTH * minimized_x
+                            + 1; //for drawing border
+                var pos_y = cluster.Average(p => p.Y) / SCREEN_HEIGHT * minimized_y
+                            + clusters.Count + 2; //writes out info of clusters and then illustrates
+                Console.SetCursorPosition((int)pos_x, (int)pos_y);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write('X');
+                Console.ResetColor();
+            }
+
+            for (int i = 1; i < minimized_y; i++)
+            {
+                Console.SetCursorPosition(0, clusters.Count + 2 + i);
+                Console.Write('|');
+                Console.SetCursorPosition(minimized_x, clusters.Count + 2 + i);
+                Console.Write('|');
+            }
+
+            for (int i = 1; i < minimized_x; i++)
+            {
+                Console.SetCursorPosition(i, clusters.Count + 2);
+                Console.Write('-');
+                Console.SetCursorPosition(i, clusters.Count + 2 + minimized_y);
+                Console.Write('-');
             }
         }
 
-        public static List<HashSet<int[]>> DBSCAN(int[][] clicks, int minPts, double eps)
+        public static List<HashSet<Point>> DBSCAN(Point[] clicks, int minPts, double eps)
         {
-            List<HashSet<int[]>> clusters = [];
-            HashSet<int[]> Processed = [];
+            List<HashSet<Point>> clusters = [];
+            HashSet<Point> processed = [];
 
-            foreach (int[] p in clicks)
+            foreach (Point p in clicks)
             {
-                if (!Processed.Contains(p))
+                if (!processed.Contains(p))
                 {
-                    HashSet<int[]> Neighbours = clicks.Where(other => Ds(p, other) <= eps).ToHashSet();
-                    if (Neighbours.Count >= minPts)
+                    HashSet<Point> neighbours = clicks.Where(other => Ds(p, other) <= eps).ToHashSet();
+                    if (neighbours.Count >= minPts)
                     {
-                        HashSet<int[]> Reachable = [];
-                        foreach (int[] q in Neighbours)
+                        HashSet<Point> cluster = [];
+                        Queue<Point> queue = new(neighbours);
+
+                        while (queue.Count > 0)
                         {
-                            Processed.Add(q);
-                            Reachable.Add(q);
-                            HashSet<int[]> ClosePoints = clicks.Where(other => Ds(q, other) <= eps).ToHashSet();
-                            if (ClosePoints.Count >= minPts)
+                            Point q = queue.Dequeue();
+                            if (!processed.Contains(q))
                             {
-                                Neighbours.UnionWith(ClosePoints); // ez problem
+                                processed.Add(q);
+                                cluster.Add(q);
+                                HashSet<Point> newNeighbours = clicks.Where(other => Ds(q, other) <= eps).ToHashSet();
+                                if (newNeighbours.Count >= minPts)
+                                {
+                                    foreach (Point n in newNeighbours)
+                                    {
+                                        if (!processed.Contains(n))
+                                        {
+                                            queue.Enqueue(n);
+                                        }
+                                    }
+                                }
                             }
                         }
-                        clusters.Add(Reachable);
+                        clusters.Add(cluster);
                     }
                 }
             }
             return clusters;
         }
 
-        private static double Ds(int[] p, int[] other) => Math.Sqrt(Math.Pow(p[0] - other[0], 2) + Math.Pow(p[1] - other[1], 2));
+        private static double Ds(Point p, Point other) => Math.Sqrt(Math.Pow(p.X - other.X, 2) + Math.Pow(p.Y - other.Y, 2));
+    }
+
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Point point
+                    && point.X == X
+                    && point.Y == Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
     }
 }
